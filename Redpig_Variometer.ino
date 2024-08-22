69887baefb0bf179438b2b509ca6cb6cc5c5fbb1
javiernetes <xavi@santmodest.net>
2024-08-22 15:01:33 +0200

    Update Redpig_Variometer.ino
    cables y setup

--- Redpig_Variometer.ino
+++ Redpig_Variometer.ino
@@ -12,23 +12,23 @@
 //     ---
 //      D
 
-//WIFI
+//WIFI______________________________________________________________________________________
 #include <WiFi.h>        // For connecting ESP32 to WiFi
 const char* ssid = "Your SSID";  // Change to your WiFi Network name
 const char* password = "You Password";  // Change to your password
-//OTA
+//OTA_______________________________________________________________________________________
 #include <ArduinoOTA.h>  // For enabling over-the-air updates
-//Display  4 DIGIT LCD
+//Display  4 DIGIT LCD 7 SEGMENTS __________________________________________________________
 #include <TM1637Display.h>
-#define CLK 2
-#define DIO 3
+#define CLK 7
+#define DIO 6
 M1637Display display(CLK, DIO); // Create an instance of the TM1637Display
-//Rotarry encoder 
+//Rotarry encoder __________________________________________________________________________
 //Defining pins
 //Arduino interrupt pins: 4, 5.
-const int RotaryCLK = PB3; //CLK pin on the rotary encoder
-const int RotaryDT = PB4; //DT pin on the rotary encoder
-const int PushButton = PB5; //Button to enter/exit menu
+const int RotaryCLK = 38; //CLK pin on the rotary encoder
+const int RotaryDT = 37; //DT pin on the rotary encoder
+const int PushButton = 35; //Button to enter/exit menu
 //Statuses for the rotary encoder
 int CLKNow;
 int CLKPrevious;
@@ -36,13 +36,16 @@ int DTNow;
 int DTPrevious;
 bool refreshLCD = true; //refreshes values
 bool refreshSelection = false; //refreshes selection (> / X)
-//Sensor Presión atmosférico
+//Sensor Presión atmosférico ______________________________________________________________
 #include <Wire.h>
 #include <ErriezBMX280.h>
 #define SEA_LEVEL_PRESSURE_HPA      1026.25 //Adjust sea level for altitude calculation
 ErriezBMX280 bmx280 = ErriezBMX280(0x76); ////Create BMX280 object I2C address 0x76 or 0x77
-
-
+#include <FastLED.h>______________________________________________________________________
+#define NUM_LEDS 10   // Numero de leds
+#define PIN  13      // PIN ARDUINO
+CRGB leds[NUM_LEDS];
+// PALABRAS_______________________________________________________________________________
 const uint8_t celsius[] = {
   SEG_A | SEG_B | SEG_F | SEG_G,  // Circle
   SEG_A | SEG_D | SEG_E | SEG_F   // C
@@ -65,21 +68,22 @@ const uint8_t data[] = { // -REDPIG-ALTIMETER
   };
 
 
-//___________SETUP
+//######################SETUP##########################################################
 void setup() {
+  ArduinoOTA.begin();  // Starts OTA ________________por si upload firmware waiting at boot
+  ArduinoOTA.handle();  // Handles a code update request
+  Serial.begin(9600);
+  Serial.println(F("SETUP Peripherals"));//____________________________________________
   display.clear();    
   display.setBrightness(5);  // Set the display brightness (0-7)
   for (int i=0; i <= 9999; i++){
      display.showNumberDec(i, true);  // i=value, true to padd the blank segments with 0
      delay(10);
   };
-
-  // -
   for ( int i=0; i<(sizeof(data)-3); i++ ) {
     display.setSegments(data+i); // Display the data
     delay(500);
   };
-
   ////// Blink String
   for ( int i=0; i<5; i++ ) {
     display.setSegments(data+4); // Display the data
@@ -87,25 +91,44 @@ void setup() {
     display.clear();             // Clear the display
     delay(250);
   };
-    
+  //    SETUP WIFI ___________________________________________________________  
   WiFi.begin(ssid, password);  // Connect to WiFi - defaults to WiFi Station mode
-
   // Ensure WiFi is connected
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-
-  // Initialize serial
-    delay(500);
-    Serial.begin(115200);
-    while (!Serial) {
-        ;
-    }
-    Serial.println(F("\nErriez BMP280/BMX280 example"));
+    Serial.println(F("SETUP WIFI"));
   }
-
-  ArduinoOTA.begin();  // Starts OTA
+  //_ SETUP BMP280________________________________________________________________
+    Serial.println(F("\nErriez BMP280 SETUP"))
+  bool status;
+  // default settings
+  // (you can also pass in a Wire library object like &Wire2)
+  status = bme.begin(0x76);  
+  if (!status) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1);
+  }
+  //FAST LEDS _____SETUP
+  Serial.println("-- Default WP2812B led strip--");
+  Serial.println();
+  FastLED.addLeds<WS2812B, PIN, GRB>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.clear(); 
+  Try
+  fill_solid(leds, NUM_LEDS, CRGB::Blue);
+  FastLED.show();
+  delay(500);
+  fill_solid(leds, NUM_LEDS, CRGB::Red);
+  FastLED.show();
+  delay(500);
+  fill_solid(leds, NUM_LEDS, CRGB::Orange);
+  FastLED.show();
+  delay(500);
+  fill_solid(leds, NUM_LEDS, CRGB::Green);
+  FastLED.show();
+  delay(500);
 }
-
+//####################################################################################################
+//________________________LOOP########################################################################
 void loop() {
 
   ArduinoOTA.handle();  // Handles a code update request
